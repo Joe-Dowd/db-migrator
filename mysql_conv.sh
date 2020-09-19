244746fd59b1eb2cdad5c319909d404b84ea3c9f
Joe Dowd <joedowd96@gmail.com>
2020-09-19 12:40:34 +0100

    updated upgrade

--- mysql_conv.sh
+++ mysql_conv.sh
@@ -23,12 +23,14 @@ DROP TABLE IF EXISTS sponsorblock.config;
 DROP TABLE IF EXISTS sponsorblock.sponsorTimes;
 DROP TABLE IF EXISTS sponsorblock.userNames;
 DROP TABLE IF EXISTS sponsorblock.vipUsers;
+DROP TABLE IF EXISTS sponsorblock.noSegments;
 
 RENAME TABLE sponsorblock_staging.categoryVotes TO sponsorblock.categoryVotes;
 RENAME TABLE sponsorblock_staging.config TO sponsorblock.config;
 RENAME TABLE sponsorblock_staging.sponsorTimes TO sponsorblock.sponsorTimes;
 RENAME TABLE sponsorblock_staging.userNames TO sponsorblock.userNames;
 RENAME TABLE sponsorblock_staging.vipUsers TO sponsorblock.vipUsers;
+RENAME TABLE sponsorblock_staging.noSegments TO sponsorblock.noSegments;
 COMMIT;
 EOF
 
@@ -36,7 +38,7 @@ curl --show-error --fail https://sponsor.ajay.app/database.db > latest.db
 echo 'Downloaded database with no error'
 ls -l
 
-sqlite3 latest.db .dump | ./sqlite3-to-mysql.py | sed 's/\sTEXT/ VARCHAR(255)/g' | sed "s/\sDEFAULT\s\`sponsor\`/ DEFAULT 'sponsor'/g" > mysql.sql
+sqlite3 latest.db .dump | ./sqlite3-to-mysql.py | sed 's/\sTEXT/ VARCHAR(255)/g' | sed "s/\sDEFAULT\s\`sponsor\`/ DEFAULT 'sponsor'/g" | sed "s/\sdefault\s\`\`/ default ''/g" > mysql.sql
 echo 'Converted to mysql'
 
 mysql -h mysql -u sponsorblock sponsorblock_staging < clean.sql
